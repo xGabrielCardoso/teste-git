c90bb73008954ebf3aac31bc7f075a2f5dabf3e1
Gabriel <ggomes847@gmail.com>
2019-07-22 17:00:01 -0300

    Acrescentado comentarios

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Projeto_Teste
+namespace Projeto_Teste // ORGANIZAÇÃO , PARA ENCONTRAR AS CLASSES COM FACILIDADE NO PROJETO
 {
-    class Program
+    class Program //NOME DA CLASSE
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Entrepoint = vai ser carregado primeiro de tudo assim como o Load do 4Manager
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Fala Monstroooo?!!");
