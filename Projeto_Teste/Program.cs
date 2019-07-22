25dd868f6f5debb2f7d9b022a13d6633cd74ca4c
Gabriel <ggomes847@gmail.com>
2019-07-22 11:00:17 -0300

    Acrescentada uma mensagem de Fala Monstrooo

--- Projeto_Teste/Program.cs
+++ Projeto_Teste/Program.cs
@@ -11,6 +11,7 @@ namespace Projeto_Teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Fala Monstroooo?!!");
             Console.ReadLine();
         }
     }
